7e7feffea1a3a182d68d5df9985ebd98367d134c
Your Name <you@example.com>
2025-08-10 22:45:25 +0800

    modified

--- 09.k8s-storage/03.static-pv-use-correlation-InTreeVolumePlugin-Docking-Storage/03.cephfs-but-k8s1.28-deprecated-and-k8s1.31-removed/03.binbin-project/secrets_binbin-project-cephfs-in-binbinfs-user-key/command.sh
+++ 09.k8s-storage/03.static-pv-use-correlation-InTreeVolumePlugin-Docking-Storage/03.cephfs-but-k8s1.28-deprecated-and-k8s1.31-removed/03.binbin-project/secrets_binbin-project-cephfs-in-binbinfs-user-key/command.sh
@@ -2,9 +2,9 @@
 ScriptDir=$(dirname "$0")
 
 # 快速编写secrets资源对象的manifests
-kubectl -n binbin  create secret  generic  binbin-project-cephfs-in-binbinfs-user-key \
-   --type=Opaque       \
+kubectl -n binbin  create secret  generic  binbin-project-ceph-fs-in-binbinfs-user-key \
+   --type=Opaque \
    --from-file=key=$ScriptDir/ceph.client.binbinfs.secret \
    --dry-run=client \
-   -o yaml >$ScriptDir/secrets_binbin-project-cephfs-in-binbinfs-user-key.yaml
+   -o yaml >$ScriptDir/secrets_binbin-project-ceph-fs-in-binbinfs-user-key.yaml
 
