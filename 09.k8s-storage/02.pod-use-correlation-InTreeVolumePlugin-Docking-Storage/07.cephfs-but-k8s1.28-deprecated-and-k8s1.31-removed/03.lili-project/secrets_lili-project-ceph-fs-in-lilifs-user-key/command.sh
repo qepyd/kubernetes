0ae989d39576ed616e78c00fbfb2e7cbca1a0fa6
Your Name <you@example.com>
2025-08-11 13:42:07 +0800

    modified

--- 09.k8s-storage/02.pod-use-correlation-InTreeVolumePlugin-Docking-Storage/07.cephfs-but-k8s1.28-deprecated-and-k8s1.31-removed/03.lili-project/secrets_lili-project-ceph-fs-in-lilifs-user-key/command.sh
+++ 09.k8s-storage/02.pod-use-correlation-InTreeVolumePlugin-Docking-Storage/07.cephfs-but-k8s1.28-deprecated-and-k8s1.31-removed/03.lili-project/secrets_lili-project-ceph-fs-in-lilifs-user-key/command.sh
@@ -2,8 +2,8 @@
 ScriptDir=$(dirname "$0")
 
 # 快速编写secrets资源对象的manifests
-kubectl -n lili  create secret  generic  lili-project-cephfs-in-lilifs-user-key \
+kubectl -n lili  create secret  generic  lili-project-ceph-fs-in-lilifs-user-key \
    --type=Opaque       \
    --from-file=key=$ScriptDir/ceph.client.lilifs.secret \
    --dry-run=client \
-   -o yaml >$ScriptDir/secrets_lili-project-cephfs-in-lilifs-user-key.yaml
+   -o yaml >$ScriptDir/secrets_lili-project-ceph-fs-in-lilifs-user-key.yaml
